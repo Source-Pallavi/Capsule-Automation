9950e48e8743d04bf1a6643c9a1f0a3eb152dc80
Source-Pallavi <59175852+Source-Pallavi@users.noreply.github.com>
2020-08-05 17:22:50 +0530

    Add project files.

---
+++ Zoopla/Test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Zoopla.Base;
+
+namespace Zoopla
+{
+    [TestFixture("chrome")]
+    [Parallelizable]
+    public class Test:BaseC
+    {
+        IWebDriver driver;
+        public Test(string browserName)
+        {
+           
+                driver = StartBrowser("chrome");
+            }
+
+                [Test]
+        public void TestNews()
+        {
+
+        }
+    }
+}
